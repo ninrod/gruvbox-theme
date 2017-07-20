75bfe91f75dc2158d267160923ce8282195fbafc
ninrod <filipe.silva@gmail.com>
2017-07-20 19:37:55 -0300

    tweak org-block

--- gruvbox-theme.el
+++ gruvbox-theme.el
@@ -391,7 +391,7 @@
 
   ;; org-mode
   (org-meta-line                             (:foreground gruvbox-zerodark-orange-light :slant 'italic))
-  (org-block                                 (:foreground gruvbox-light2 :background gruvbox-dark0_ultimate))
+  (org-block                                 (:foreground gruvbox-light2 :background gruvbox-dark0))
   (org-hide                                  (:foreground gruvbox-dark0))
   (org-level-1                               (:foreground gruvbox-spacemacs-keyword))
   (org-level-2                               (:foreground gruvbox-spacemacs-head2))
